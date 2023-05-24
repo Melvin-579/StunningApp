4bbe6d66fb05524495554d388ea73fc3198953ae
Melvin-579 <90847738+Melvin-579@users.noreply.github.com>
2023-05-24 13:22:01 -0400

    ImageCard

---
+++ app/src/main/java/com/example/stunningapp/ImageCard.kt
@@ -0,0 +1,30 @@
+package com.example.stunningapp
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import kotlin.random.Random
+@Composable
+fun ImageCard(
+    title: String,
+    description: String,
+) {
+    Card(
+        modifier = Modifier,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        ),
+        shape = MaterialTheme.shapes.large
+    ) {
+        Image(
+            painter = rememberAsy
+        ),
+    }
+}
