fd8db88833d41f4ea2101e8620b01877fc66d4a4
Melvin-579 <90847738+Melvin-579@users.noreply.github.com>
2023-05-24 17:53:33 -0400

    it runs!
    I have a better knowledge now on how jetpack compose functions using the material 3 and some of its newer implementations required!

--- app/src/main/java/com/example/stunningapp/ImageCard.kt
+++ app/src/main/java/com/example/stunningapp/ImageCard.kt
@@ -10,21 +10,118 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.ui.Alignment
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
 import kotlin.random.Random
 @Composable
+@ExperimentalLayoutApi
 fun ImageCard(
     title: String,
     description: String,
+    modifier: Modifier = Modifier
 ) {
     Card(
-        modifier = Modifier,
+        modifier = modifier,
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.surfaceVariant,
         ),
         shape = MaterialTheme.shapes.large
     ) {
         Image(
-            painter = rememberAsy
-        ),
+            painter = rememberAsyncImagePainter(
+                model = "https://picsum.photos/seed{Random.nextInt()}/300/200"
+            ),
+            contentDescription = null,
+            modifier = Modifier
+                .clip(MaterialTheme.shapes.large)
+                .fillMaxWidth()
+                .aspectRatio(3f / 2f)
+        )
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleLarge
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = description,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            FlowRow(
+                Modifier
+                    .fillMaxWidth(1f)
+                    .wrapContentHeight(align = Alignment.Top),
+                horizontalArrangement = Arrangement.spacedBy(10.dp),
+                verticalArrangement = Arrangement.spacedBy(20.dp),
+                maxItemsInEachRow = 3
+            ) {
+                repeat(10) {
+                    Box(
+                        Modifier
+                            .align(Alignment.CenterVertically)
+                            .width(50.dp)
+                            .height(50.dp)
+                            .background(Color.Green)
+                    ) {
+                       AssistChip(
+                           onClick = { },
+                           colors = AssistChipDefaults.assistChipColors(
+                               leadingIconContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                           ),
+                           leadingIcon = {
+                               Icon(
+                                   imageVector = Icons.Outlined.FavoriteBorder,
+                                   contentDescription = null
+                               )
+                           },
+                           label = {
+                               Text(text = "Mark as favorite")
+                           }
+                       )
+                        AssistChip(
+                            onClick = { },
+                            colors = AssistChipDefaults.assistChipColors(
+                                leadingIconContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                            ),
+                            leadingIcon = {
+                                Icon(
+                                    imageVector = Icons.Outlined.FavoriteBorder,
+                                    contentDescription = null
+                                )
+                            },
+                            label = {
+                                Text(text = "Share with others")
+                            }
+                        )
+                    }
+                }
+            }
+
+        }
     }
 }
+
+@Composable
+@ExperimentalLayoutApi
+inline fun FlowRow(
+    modifier: Modifier = Modifier,
+    horizontalArrangement: Arrangement.Horizontal = Arrangement.Start,
+    verticalArrangement: Arrangement.Vertical = Arrangement.Top,
+    maxItemsInEachRow: Int = Int.MAX_VALUE,
+    content: @Composable FlowRowScope.() -> Unit
+): Unit{}
--- app/src/main/java/com/example/stunningapp/MainActivity.kt
+++ app/src/main/java/com/example/stunningapp/MainActivity.kt
@@ -3,16 +3,22 @@ package com.example.stunningapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.stunningapp.ui.theme.StunningAppTheme
 
+@OptIn(ExperimentalLayoutApi::class)
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +28,38 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    Scaffold(
+                        floatingActionButton = {
+                            FloatingActionButton(onClick = { /*TODO*/ }) {
+                                Icon(
+                                    imageVector = Icons.Default.Add,
+                                    contentDescription = null,
+                                    tint = MaterialTheme.colorScheme.onSecondaryContainer
+                                )
+                            }
+                        },
+                        topBar = {
+                            SmallTopAppBar(
+                                title = {
+                                    Text(text = "Material 3")
+                                },
+                                colors = TopAppBarDefaults.smallTopAppBarColors(
+                                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                                    titleContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                                )
+                            )
+                        }
+                    ) {
+                        LazyColumn{
+                            items(20){
+                                ImageCard(
+                                    title = "Here we are",
+                                    description = "It RUNS!",
+                                    modifier = Modifier.padding(16.dp)
+                                )
+                            }
+                        }
+                    }
                 }
             }
         }
